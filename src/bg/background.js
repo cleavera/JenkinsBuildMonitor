d17f0bb7de1417fcbe189c8f160212add794d044
antisback <antisback@gmail.com>
2014-12-12 15:13:09 +0000

    Separating out the notifications service

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,57 +1,8 @@
 (function (module) {
-  module.factory('backgroundService', ['BuildModel', 'Persistance', function (BuildModel, Persistance) {
-    var model = {},
-      streams = Persistance.Get('SubscribedStreams') || [];
-
-    var notifications = function (title, status, blame) {
-      chrome.notifications.create('', {
-        type: 'basic', 
-        iconUrl: '/icons/' + status + '.png',
-        title: 'Build '+ status, 
-        message: title,
-        contextMessage: blame
-      }, function () {});
-    };
-
-    var parseBuilds = function (data) {
-      var build,
-        blame = '',
-        name = data.name;
-
-      if (data.culprits[0]) {
-        blame = data.culprits[0].fullName;
-      }
-
-      build = {
-        blame: blame.toLowerCase(),
-        url: data.url,
-        status: data.building ? 'started' : data.result === 'SUCCESS' ? 'succeeded' : 'failed'
-      };
-
-      if(model[name] && model[name].status != build.status) {
-        notifications(name, build.status, build.blame);
-      }
-
-      model[name] = build;
-    };
-
-    var getBuildStatus = function () {
-      var x;
-
-      for (x = 0; x < streams.length; x++) {
-        BuildModel.GetLast(streams[x]).then(parseBuilds);
-      }
-    };
-
-    return {
-      notifications: getBuildStatus
-    };
-  }]);
-
-  module.run(['$timeout', 'backgroundService', function ($timeout, backgroundService) {
+  module.run(['$timeout', 'BuildNotificationService', function ($timeout, BuildNotificationService) {
     var go = function () {
       $timeout(function () {
-        backgroundService.notifications();
+        BuildNotificationService.Notifications();
         go();
       }, 10000);
     };
--- src/bg/notifications.js
+++ src/bg/notifications.js
@@ -1,7 +1,50 @@
 (function (module) {
-  'use strict';
+  module.factory('BuildNotificationService', ['BuildModel', 'Persistance', function (BuildModel, Persistance) {
+    var model = {},
+      streams = Persistance.Get('SubscribedStreams') || [];
 
-  ngModule.factory('nameFactory', [function () {
-    
+    var notifications = function (title, status, blame) {
+      chrome.notifications.create('', {
+        type: 'basic', 
+        iconUrl: '/icons/' + status + '.png',
+        title: 'Build '+ status, 
+        message: title,
+        contextMessage: blame
+      }, function () {});
+    };
+
+    var parseBuilds = function (data) {
+      var build,
+        blame = '',
+        name = data.name;
+
+      if (data.culprits[0]) {
+        blame = data.culprits[0].fullName;
+      }
+
+      build = {
+        blame: blame.toLowerCase(),
+        url: data.url,
+        status: data.building ? 'started' : data.result === 'SUCCESS' ? 'succeeded' : 'failed'
+      };
+
+      if(model[name] && model[name].status != build.status) {
+        notifications(name, build.status, build.blame);
+      }
+
+      model[name] = build;
+    };
+
+    var getBuildStatus = function () {
+      var x;
+
+      for (x = 0; x < streams.length; x++) {
+        BuildModel.GetLast(streams[x]).then(parseBuilds);
+      }
+    };
+
+    return {
+      Notifications: getBuildStatus
+    };
   }]);
-})(angular.module('app.module'));
+}(angular.module('monitor.background')));
